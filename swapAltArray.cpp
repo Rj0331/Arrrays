acd98975639265bbed06936b0fb652fbd5b8269b
Rahul Kumar <89581367+Rj0331@users.noreply.github.com>
2022-07-23 17:30:55 +0530

    Create swapAltArray.cpp

---
+++ swapAltArray.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int a[], int n)
+{
+    int start = 1;
+    int end = n -2 ;
+    while (start <= end)
+    {
+        swap(a[start], a[end]);
+        start+=2;
+        end-=2;
+    }
+}
+void printArray(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout <<  a[i]<< " ";
+    }
+    cout<< endl;
+}
+
+int main()
+{
+    int arr[6] = {0, 4, 56, 32, 44, 5};
+    int brr[5] = {7, 88, 54, 2, 3};
+
+    reverse(arr, 6);
+
+    reverse(brr, 5);
+
+    printArray(arr, 6); 
+
+    printArray(brr, 5);
+    return 0;
+}
